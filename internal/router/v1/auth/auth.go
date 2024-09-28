4e400701dd03826d5662f60d3c4bb645eff28e0a
muhammadabdulazizalghofari <muhammadabdulazizalghofari@gmail.com>
2024-09-28 08:29:11 +0000

    feat: Disabling Debug path on prod

--- internal/router/v1/auth/auth.go
+++ internal/router/v1/auth/auth.go
@@ -3,12 +3,15 @@ package auth
 import (
 	"backend/internal/handler"
 	"net/http"
+	"os"
 )
 
 func InitRouter(h *handler.Handlers) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/google/login", h.Auth.GoogleLogin)
 	mux.HandleFunc("/google/callback", h.Auth.GoogleCallback)
-	mux.HandleFunc("POST /debug/login", h.Auth.DevLogin)
+	if os.Getenv("ENV") == "dev" {
+		mux.HandleFunc("POST /debug/login", h.Auth.DevLogin)
+	}
 	return http.StripPrefix("/auth", mux)
 }
