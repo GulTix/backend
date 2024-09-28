7f0c5191dcad7f602939189b294f4ad7fa1e4eb3
muhammadabdulazizalghofari <muhammadabdulazizalghofari@gmail.com>
2024-09-28 08:50:52 +0000

    feat: Adjust migration like Exclidraw Schema

--- pkg/migrations/query/000002_validation table.down.sql
+++ pkg/migrations/query/000002_validation table.down.sql
@@ -1,1 +1,10 @@
+-- Remove the bevy_link column from ticket_type table
+ALTER TABLE ticket_type
+DROP COLUMN bevy_link;
+
+-- Add bevy_link column back to events table
+ALTER TABLE events
+ADD COLUMN bevy_link VARCHAR NOT NULL  -- Adjust the data type as needed
+
+-- Drop the validations table
 DROP TABLE IF EXISTS validations;
--- pkg/migrations/query/000002_validation table.up.sql
+++ pkg/migrations/query/000002_validation table.up.sql
@@ -1,11 +1,20 @@
--- CREATE TABLE IF NOT EXISTS validations (
---     id VARCHAR PRIMARY KEY,
---     answer_id VARCHAR,
---     user_id VARCHAR,
---     volunteer_id VARCHAR,
---     deleted BOOLEAN DEFAULT FALSE,
---     classification VARCHAR NOT NULL,
---     FOREIGN KEY (answer_id) REFERENCES answers(id),
---     FOREIGN KEY (user_id) REFERENCES users(id),
---     FOREIGN KEY (volunteer_id) REFERENCES volunteers(id)
--- );
+CREATE TABLE IF NOT EXISTS validations (
+    id VARCHAR PRIMARY KEY,
+    answer_id VARCHAR,
+    user_id VARCHAR,
+    volunteer_id VARCHAR,
+    deleted BOOLEAN DEFAULT FALSE,
+    payment_email_sent BOOLEAN DEFAULT FALSE,
+    classification VARCHAR NOT NULL,
+    FOREIGN KEY (answer_id) REFERENCES answers(id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (volunteer_id) REFERENCES volunteers(id)
+);
+
+-- Drop column bevy_link from events table
+ALTER TABLE events
+DROP COLUMN bevy_link;
+
+-- Add column bevy_link to ticket_type table
+ALTER TABLE ticket_type
+ADD COLUMN bevy_link VARCHAR NOT NULL;  
